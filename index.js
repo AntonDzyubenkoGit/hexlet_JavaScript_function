b5cbc66e093c2607e069f85be8172ed650e45b2b
AntonDzyubenkoGit <107909682+AntonDzyubenkoGit@users.noreply.github.com>
2023-02-14 12:20:03 +0300

    Внутренняя функция takeLast(), которая возвращает последние n символов строки в обратном порядке.

--- index.js
+++ index.js
@@ -1,21 +1,42 @@
 import _ from "lodash";
 
-// Функция convert(), которая конвертирует даты в массив человеко-читаемых строк на английском языке
+// Внутренняя функция takeLast(), которая возвращает последние n символов строки в обратном порядке.
 
-const convert = (...args) => {
-  const result = [];
+const run1 = (text) => {
+  const takeLast = (text, count) => {
+    if (count > text.length) {
+      return null;
+    }
 
-  for (let i = 0; i < args.length; i += 1) {
-    const date = new Date(...args[i]).toDateString();
-    result.push(date);
-  }
-  return result;
+    let result = "";
+    const lBound = text.length - count;
+
+    for (let i = text.length - 1; i >= lBound; i -= 1) {
+      result = `${result}${text[i]}`;
+    }
+    return result;
+  };
+
+  return takeLast(text, 4);
 };
 
-const date = [
-  [1993, 3, 24],
-  [1997, 8, 12],
-  [2001, 10, 18],
-];
+// Вариант через массив и преобразование его в строку
 
-console.log(convert(...date));
+const run2 = (text) => {
+  const takeLast = (str, length) => {
+    if (str.length === 0 || str.length < length) {
+      return null;
+    }
+
+    const result = [];
+
+    for (let i = str.length - 1; result.length < length; i -= 1) {
+      result.push(str[i]);
+    }
+    return result.join("");
+  };
+
+  return takeLast(text, 4);
+};
+
+console.log(run1("kids"));
