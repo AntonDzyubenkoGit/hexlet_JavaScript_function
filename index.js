0e662566a93f9adf6eb674583311ce1f2f004cc1
AntonDzyubenkoGit <107909682+AntonDzyubenkoGit@users.noreply.github.com>
2023-02-14 18:09:31 +0300

    Функция takeOldest(), которая принимает на вход список пользователей и возвращает самых взрослых.

--- index.js
+++ index.js
@@ -1,42 +1,56 @@
 import _ from "lodash";
 
-// Внутренняя функция takeLast(), которая возвращает последние n символов строки в обратном порядке.
+// Функция takeOldest(), которая принимает на вход список пользователей и возвращает самых взрослых.
 
-const run1 = (text) => {
-  const takeLast = (text, count) => {
-    if (count > text.length) {
-      return null;
-    }
-
-    let result = "";
-    const lBound = text.length - count;
-
-    for (let i = text.length - 1; i >= lBound; i -= 1) {
-      result = `${result}${text[i]}`;
-    }
-    return result;
-  };
-
-  return takeLast(text, 4);
+const takeOldest = (users, count = 1) => {
+  const sorted = _.sortBy(users, ({ birthday }) => Date.parse(birthday));
+  return sorted.slice(0, count);
 };
 
-// Вариант через массив и преобразование его в строку
+// Версия через встроенный .sort()
 
-const run2 = (text) => {
-  const takeLast = (str, length) => {
-    if (str.length === 0 || str.length < length) {
-      return null;
-    }
-
-    const result = [];
-
-    for (let i = str.length - 1; result.length < length; i -= 1) {
-      result.push(str[i]);
-    }
-    return result.join("");
-  };
-
-  return takeLast(text, 4);
+const takeOldest2 = (users, num = 1) => {
+  users.sort(function (a, b) {
+    return new Date(a.birthday) - new Date(b.birthday);
+  });
+  return users.slice(0, num);
 };
 
-console.log(run1("kids"));
+// Версия через встроенный .sort() Math.sign
+
+const takeOldest3 = (users, num = 1) => {
+  users.sort((a, b) => Math.sign(new Date(a.birthday) - new Date(b.birthday)));
+  return users.slice(0, num);
+};
+
+// Версия без приведенного формата даты
+
+const takeOldest4 = (users, count = 1) => {
+  for (const item of users) {
+    item.birthday = Date.parse(item.birthday);
+  }
+
+  const sortUsers = _.sortBy(users, "birthday");
+
+  for (const obj of sortUsers) {
+    obj.birthday = new Date(obj.birthday).toDateString();
+  }
+
+  const result = [];
+  for (let i = 0; i < count; i++) {
+    result.push(sortUsers[i]);
+  }
+
+  return result;
+};
+
+const users = [
+  { name: "Tirion", birthday: "Nov 19, 1988" },
+  { name: "Sam", birthday: "Nov 22, 1999" },
+  { name: "Rob", birthday: "Jan 11, 1975" },
+  { name: "Sansa", birthday: "Mar 20, 2001" },
+  { name: "Tisha", birthday: "Feb 27, 1992" },
+  { name: "Chris", birthday: "Dec 25, 1995" },
+];
+
+console.log(takeOldest3(users, 2));
