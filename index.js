81a06f7b4077b74493612c1bd599dac0a579dd16
AntonDzyubenkoGit <107909682+AntonDzyubenkoGit@users.noreply.github.com>
2023-02-15 12:16:37 +0300

    Функция getChildren(), которая принимает на вход список пользователей и возвращает плоский список их детей

--- index.js
+++ index.js
@@ -1,56 +1,30 @@
 import _ from "lodash";
 
-// Функция takeOldest(), которая принимает на вход список пользователей и возвращает самых взрослых.
+// Функция getChildren(), которая принимает на вход список пользователей и возвращает плоский список их детей
 
-const takeOldest = (users, count = 1) => {
-  const sorted = _.sortBy(users, ({ birthday }) => Date.parse(birthday));
-  return sorted.slice(0, count);
-};
-
-// Версия через встроенный .sort()
-
-const takeOldest2 = (users, num = 1) => {
-  users.sort(function (a, b) {
-    return new Date(a.birthday) - new Date(b.birthday);
-  });
-  return users.slice(0, num);
-};
-
-// Версия через встроенный .sort() Math.sign
-
-const takeOldest3 = (users, num = 1) => {
-  users.sort((a, b) => Math.sign(new Date(a.birthday) - new Date(b.birthday)));
-  return users.slice(0, num);
-};
-
-// Версия без приведенного формата даты
-
-const takeOldest4 = (users, count = 1) => {
-  for (const item of users) {
-    item.birthday = Date.parse(item.birthday);
-  }
-
-  const sortUsers = _.sortBy(users, "birthday");
-
-  for (const obj of sortUsers) {
-    obj.birthday = new Date(obj.birthday).toDateString();
-  }
-
-  const result = [];
-  for (let i = 0; i < count; i++) {
-    result.push(sortUsers[i]);
-  }
-
+const getChildren = (collection) => {
+  const result = collection.map(({ children }) => children).flat();
   return result;
 };
 
+
 const users = [
-  { name: "Tirion", birthday: "Nov 19, 1988" },
-  { name: "Sam", birthday: "Nov 22, 1999" },
-  { name: "Rob", birthday: "Jan 11, 1975" },
-  { name: "Sansa", birthday: "Mar 20, 2001" },
-  { name: "Tisha", birthday: "Feb 27, 1992" },
-  { name: "Chris", birthday: "Dec 25, 1995" },
+  {
+    name: "Tirion",
+    children: [{ name: "Mira", birthday: "1983-03-23" }],
+  },
+  { name: "Bronn", children: [] },
+  {
+    name: "Sam",
+    children: [
+      { name: "Aria", birthday: "2012-11-03" },
+      { name: "Keit", birthday: "1933-05-14" },
+    ],
+  },
+  {
+    name: "Rob",
+    children: [{ name: "Tisha", birthday: "2012-11-03" }],
+  },
 ];
 
-console.log(takeOldest3(users, 2));
+console.log(getChildren(users));
