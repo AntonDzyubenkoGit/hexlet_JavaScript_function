4b45a76953f2a0c3fb730e1e43ee9b786a1d34ea
AntonDzyubenkoGit <107909682+AntonDzyubenkoGit@users.noreply.github.com>
2023-02-18 14:23:31 +0300

    Функция getFreeDomainsCount(). Добавлен второй вариант решения

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 import _ from "lodash";
 import path from "path";
 import * as fs from "node:fs";
+import get from "lodash/get.js";
 
-// Функция getFreeDomainsCount(), которая принимает на вход список емейлов, а возвращает количество емейлов, расположенных на каждом бесплатном домене. 
+// Функция getFreeDomainsCount(), которая принимает на вход список емейлов, а возвращает количество емейлов, расположенных на каждом бесплатном домене.
 
 const emails = ["info@gmail.com", "info@yandex.ru", "info@hotmail.com", "mk@host.com", "support@hexlet.io", "key@yandex.ru", "sergey@gmail.com", "vovan@gmail.com", "vovan@hotmail.com"];
 const freeEmailDomains = ["gmail.com", "yandex.ru", "hotmail.com", "yahoo.com"];
@@ -22,4 +23,18 @@ const getFreeDomainsCount = (emails) => {
 
 console.log(getFreeDomainsCount(emails));
 
+// Вариант с использованием _.get()
 
+const getFreeDomainsCount2 = (emails) =>
+  emails
+    .map((email) => {
+      const [, domain] = email.split("@");
+      return domain;
+    })
+    .filter((domain) => freeEmailDomains.includes(domain))
+    .reduce((acc, domain) => {
+      const count = get(acc, domain, 0) + 1;
+      return { ...acc, [domain]: count };
+    }, {});
+
+console.log(getFreeDomainsCount2(emails));
