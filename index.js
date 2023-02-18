7883ab8cf19022c48e3736ea855fb3848013bf5b
AntonDzyubenkoGit <107909682+AntonDzyubenkoGit@users.noreply.github.com>
2023-02-18 11:20:13 +0300

    Функция groupBy(), которая группирует объекты по заданному свойству. Новая версия

--- index.js
+++ index.js
@@ -1,57 +1,21 @@
-import _ from "lodash";
+// import _ from "lodash";
 
 // Функция groupBy(), которая группирует объекты по заданному свойству
+const students = [
+  { name: "Tirion", class: "B", mark: 3 },
+  { name: "Keit", class: "A", mark: 3 },
+  { name: "Ramsey", class: "A", mark: 4 },
+];
 
 const groupBy = (objects, key) =>
   objects.reduce((acc, object) => {
-    const groupName = object[key];
-    const group = acc[groupName] ?? [];
-    return { ...acc, [groupName]: group.concat(object) };
-  }, {});
-
-//Вариант с lodash
-
-const groupBy2 = (collection, property) => {
-  const result = _.groupBy(collection, property);
-  return result;
-};
-
-// Версия №3
-
-const groupBy3 = (collection, key) => {
-  const result = collection.reduce((a, b) => {
-    (a[b[key]] = a[b[key]] || []).push(b);
-    return a;
-  }, {});
-  return result;
-};
-
-//Версия №4
-const groupBy4 = (arrayObj, property) => {
-  const gB = (acc, obj) => {
-    const value = obj[property];
-
-    if (!Object.hasOwn(acc, value)) {
-      acc[value] = [];
+    if (!Object.hasOwn(acc, object[key])) {
+      acc[object[key]] = [];
     }
 
-    acc[value].push({ ...obj });
+    acc[object[key]].push(object);
 
     return acc;
-  };
-  return arrayObj.reduce(gB, {});
-};
+  }, {});
 
-const students = [
-  { name: "Tirion", class: "B", mark: 2 },
-  { name: "Keit", class: "A", mark: 3 },
-  { name: "Ramsey", class: "A", mark: 4 },
-  { name: "Bronn", class: "B", mark: 3 },
-  { name: "Sam", class: "A", mark: 2 },
-  { name: "Aria", class: "B", mark: 5 },
-  { name: "Keit", class: "B", mark: 4 },
-  { name: "Rob", class: "B", mark: 4 },
-  { name: "Taywin", class: "A", mark: 5 },
-];
-
-console.log(groupBy4(students, "class"));
+console.log(groupBy(students, "mark"));
