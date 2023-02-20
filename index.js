fdb56c041c34ba4b5ca2c1073b517b7c68ee3132
AntonDzyubenkoGit <107909682+AntonDzyubenkoGit@users.noreply.github.com>
2023-02-20 12:22:04 +0300

    Испытания. Javascript: Счётчик одногодок

--- index.js
+++ index.js
@@ -1,23 +1,45 @@
-// import _ from "lodash";
+import _ from "lodash";
 // import path from "path";
 // import * as fs from "node:fs";
 // import get from "lodash/get.js";
 
-// Испытания. Javascript: Переворот строки
+// Испытания. Javascript: Счётчик одногодок
 
-const reverse = (str) => {
-  const [...result] = str;
-  return result.reverse().join("");
+const getMenCountByYear = (coll) => {
+  const years = coll.filter(({ gender }) => gender === "male").map(({ birthday }) => birthday.slice(0, 4));
+
+  const result = years.reduce((acc, item) => {
+    acc[item] = years.filter((year) => year === item).length;
+    return acc;
+  }, {});
+
+  return result;
 };
 
-// Вариант с рекурсией
+// Вариант с lodash
 
-const reverse2 = (str) => {
-  if (str.length === 0) {
-    return str;
-  }
+const getMenCountByYear2 = (coll) => {
+  const mens = coll.filter(({ gender }) => gender === "male");
+  const years = mens.map(({ birthday }) => birthday.split("-")[0]);
+  const result = years.reduce((acc, year) => {
+    const count = _.get(acc, year, 0) + 1;
+    return { ...acc, [year]: count };
+  }, {});
 
-  return `${str.slice(-1)}${reverse2(str.slice(0, - 1))}`
+  return result;
 };
 
-console.log(reverse2("hexlet"));
+const users = [
+  { name: "Bronn", gender: "male", birthday: "1973-03-23" },
+  { name: "Reigar", gender: "male", birthday: "1973-11-03" },
+  { name: "Eiegon", gender: "male", birthday: "1963-11-03" },
+  { name: "Sansa", gender: "female", birthday: "2012-11-03" },
+  { name: "Jon", gender: "male", birthday: "1980-11-03" },
+  { name: "Robb", gender: "male", birthday: "1980-05-14" },
+  { name: "Tisha", gender: "female", birthday: "2012-11-03" },
+  { name: "Rick", gender: "male", birthday: "2012-11-03" },
+  { name: "Joffrey", gender: "male", birthday: "1999-11-03" },
+  { name: "Edd", gender: "male", birthday: "1973-11-03" },
+];
+
+console.log(getMenCountByYear2(users));
