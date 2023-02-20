69ab1b62a38f5811b0f94155c56dccd69940ce52
AntonDzyubenkoGit <107909682+AntonDzyubenkoGit@users.noreply.github.com>
2023-02-20 08:28:47 +0300

    Функция sequenceSum(), которая которая находит сумму последовательности целых чисел.

--- index.js
+++ index.js
@@ -1,40 +1,19 @@
-import _ from "lodash";
-import path from "path";
-import * as fs from "node:fs";
-import get from "lodash/get.js";
+// import _ from "lodash";
+// import path from "path";
+// import * as fs from "node:fs";
+// import get from "lodash/get.js";
 
-// Функция getFreeDomainsCount(), которая принимает на вход список емейлов, а возвращает количество емейлов, расположенных на каждом бесплатном домене.
+// Функция sequenceSum(), которая которая находит сумму последовательности целых чисел.
 
-const emails = ["info@gmail.com", "info@yandex.ru", "info@hotmail.com", "mk@host.com", "support@hexlet.io", "key@yandex.ru", "sergey@gmail.com", "vovan@gmail.com", "vovan@hotmail.com"];
-const freeEmailDomains = ["gmail.com", "yandex.ru", "hotmail.com", "yahoo.com"];
+const sequenceSum = (begin, end) => {
+  if (end === begin) {
+    return begin;
+  }
+  if (end < begin) {
+    return NaN;
+  }
 
-const getFreeDomainsCount = (emails) => {
-  const domains = emails.map((mail) => mail.split("@")[1]);
-
-  const result = freeEmailDomains.reduce((acc, item) => {
-    if (domains.includes(item)) {
-      acc[item] = domains.filter((domain) => domain === item).length;
-    }
-    return acc;
-  }, {});
-
-  return result;
+  return end + sequenceSum(begin, end - 1);
 };
 
-console.log(getFreeDomainsCount(emails));
-
-// Вариант с использованием _.get()
-
-const getFreeDomainsCount2 = (emails) =>
-  emails
-    .map((email) => {
-      const [, domain] = email.split("@");
-      return domain;
-    })
-    .filter((domain) => freeEmailDomains.includes(domain))
-    .reduce((acc, domain) => {
-      const count = get(acc, domain, 0) + 1;
-      return { ...acc, [domain]: count };
-    }, {});
-
-console.log(getFreeDomainsCount2(emails));
+console.log(sequenceSum(1, 5));
