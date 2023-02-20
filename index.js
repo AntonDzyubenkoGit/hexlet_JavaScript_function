6a69737d67e5f5001f35013eeb117d3a2e87544f
AntonDzyubenkoGit <107909682+AntonDzyubenkoGit@users.noreply.github.com>
2023-02-20 11:11:30 +0300

    Испытания. Javascript: Одинаковая четность

--- index.js
+++ index.js
@@ -3,20 +3,27 @@
 // import * as fs from "node:fs";
 // import get from "lodash/get.js";
 
-// Функция smallestDivisor(), которая находит наименьший делитель заданного числа
+// Испытания. Javascript: Одинаковая четность
 
-const smallestDivisor = (num) => {
-  if (num === 1) {
-    return num;
+const isEven = (num) => num % 2 === 0;
+
+const sameParityFilter = (collection) => {
+  const firstNumber = collection[0];
+
+  if (isEven(firstNumber)) {
+    return collection.filter((num) => isEven(num));
   }
-  const iter = (num, divisor) => {
-    if (num % divisor === 0) {
-      return divisor;
-    }
-    return iter(num, divisor + 1);
-  };
 
-  return iter(num, 2);
+  return collection.filter((num) => !isEven(num));
 };
 
-console.log(smallestDivisor(15));
+// Версия №2
+const sameParityFilter2 = (collection) => {
+  const firstNumber = isEven(collection[0]);
+  return collection.filter((num) => isEven(num) === firstNumber);
+};
+
+const coll1 = [-1, 0, 1, -3, 10, -2];
+const coll2 = [2, 0, 1, -3, 10, -2];
+
+console.log(sameParityFilter2(coll1));
