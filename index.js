53755caee3fe57ede4fd7f64e4a94f70df7bd2d2
AntonDzyubenkoGit <107909682+AntonDzyubenkoGit@users.noreply.github.com>
2023-02-20 20:25:28 +0300

    Испытания. Javascript: Поиск ближайшего соседа

--- index.js
+++ index.js
@@ -3,76 +3,42 @@ import _ from "lodash";
 // import * as fs from "node:fs";
 // import get from "lodash/get.js";
 
-// Испытания. Javascript: Фильтр анаграмм
+// Испытания. Javascript: Поиск ближайшего соседа
 
-const coll1 = ["aabb", "abcd", "bbaa", "dada"];
-const coll2 = ["crazer", "carer", "racar", "caers", "racer"];
-const coll3 = ["lazing", "lazy", "lacer"];
-
-const charsDictionary = (word) => {
-  const [...chars] = word;
-
-  return chars.reduce((acc, item) => {
-    acc[item] = chars.filter((char) => char === item).length;
-    return acc;
-  }, {});
-};
-
-const is = (object1, object2, keys) => {
-  for (const key of keys) {
-    if (object1[key] !== object2[key]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const filterAnagrams = (word, arr) => {
-  const result = [];
-
-  for (const item of arr) {
-    const [...keys] = item;
-    if (is(charsDictionary(word), charsDictionary(item), keys)) {
-      result.push(item);
-    }
+const findIndexOfNearest = (numbers, key) => {
+  if (numbers.length === 0) {
+    return null;
   }
 
+  const diff = numbers.map((num) => Math.abs(num - key));
+
+  let min = diff[0];
+  let result = 0;
+
+  for (let i = 0; i < "diff".length; i++) {
+    if (min > diff[i]) {
+      min = diff[i];
+      result = i;
+    }
+  }
   return result;
 };
 
-// Вариант с lodash
+// Вариант с reduce
 
-const filterAnagrams2 = (word, arr) => {
-  const [...chars] = word;
-
-  const charsDictionary = chars.reduce((acc, item) => {
-    acc[item] = chars.filter((char) => char === item).length;
-    return acc;
-  }, {});
-
-  const result = [];
-
-  for (const item of arr) {
-    const [...chars] = item;
-    const itemDictionary = chars.reduce((acc, item) => {
-      acc[item] = chars.filter((char) => char === item).length;
-      return acc;
-    }, {});
-    if (_.isEqual(charsDictionary, itemDictionary)) {
-      result.push(item);
-    }
+const findIndexOfNearest2 = (numbers, key) => {
+  if (numbers.length === 0) {
+    return null;
   }
 
-  return result;
+  const diffs = numbers.map((num) => Math.abs(num - key));
+
+  return diffs.reduce((index, diff, currrentIndex) => (diff < diffs[index] ? currrentIndex : index), 0);
 };
 
-// Вариант через filter()
+const coll = [15, 10, 3, 4];
+const coll1 = [7, 5, 3, 2];
+const coll2 = [7, 5, 4, 4, 3];
 
-const filterAnagrams3 = (word, words) => {
-  const normalize = (str) => str.split("").sort().join("");
-  const normal = normalize(word);
-
-  return words.filter((item) => normalize(item) === normal);
-};
-
-console.log(filterAnagrams3("racer", coll2));
+console.log(findIndexOfNearest(coll2, 4));
+console.log(findIndexOfNearest2(coll2, 4));
